883ec6cc1ab3c061b15c4140ca426717e71f034d
michaelnv <michaelnv@mail.ru>
2019-06-06 21:24:26 +0500

    Added clique graph calculations

---
+++ app/functions.py
@@ -0,0 +1,142 @@
+import itertools
+from typing import List, Dict, Tuple
+
+import pandas
+
+
+def get_mdnf(data_frame: pandas.DataFrame, function_name: str, variables: List[str]) -> Tuple[str, List[str]]:
+    """Calculates the minimal disjunctive normal form"""
+    vars_dict = {}
+    for index, row in data_frame.iterrows():
+        if row[function_name]:
+            var_row = {}
+            for variable in variables:
+                var_row.update({variable: row[variable]})
+            vars_dict.update({index: var_row})
+
+    #print(vars_dict)
+
+    if len(vars_dict) == len(data_frame):
+        return '1', []
+
+    implicants = find_implicants(vars_dict, variables, len(variables) - 1)
+
+    #print('implicants: ')
+    #print(implicants)
+
+    resulted_terms = find_optimal_coverage(implicants)
+    #print('resulted_terms: ')
+    #print(resulted_terms)
+
+    result_batch = []
+    for term_dict in resulted_terms:
+        row_batch = []
+        for variable, value in term_dict.items():
+            row_batch.append(variable.upper() if value else '¬' + variable.upper())
+        result_batch.append(''.join(row_batch))
+
+    return ' ∨ '.join(result_batch), result_batch
+
+
+def find_implicants(var_dict: Dict, variables: List[str], count: int) -> Dict:
+    """Finds the implicants for given terms"""
+    if count < 1:
+        return var_dict
+
+    glued_indices = []
+    implicants_dict = {}
+    #print('find_implicants: ')
+    #print(var_dict.items())
+    for index, vars_dict in var_dict.items():
+        for indx, vd in var_dict.items():
+            if vars_dict != vd:
+                matches = 0
+                matched_vars = []
+                for variable in variables:
+                    if all(key in vd.keys() for key in vars_dict) and\
+                            variable in vars_dict and vars_dict[variable] == vd[variable]:
+                        matches += 1
+                        matched_vars.append(variable)
+                if matches == count:
+                    glued_indices.append(index)
+                    glued_indices.append(indx)
+                    implicants_dict.update({(index, indx): {var: vd[var] for var in matched_vars}})
+
+    indices = set()
+    implicants_dict_pure = {}
+    for key, value in implicants_dict.items():
+        indices.update(key)
+        if value not in implicants_dict_pure.values():
+            implicants_dict_pure[tuple(sorted(key))] = value
+
+    #print('implicants_dict_pure: ')
+    #print(implicants_dict_pure)
+
+    #print('glued_indices: ')
+    glued_indices = list(set(glued_indices))
+    #print(glued_indices)
+
+    unglued_indices = set(var_dict.keys()) - set(glued_indices)
+    #print('unglued_indices: ')
+    #print(list(unglued_indices))
+    unglued_indices = tuple(unglued_indices)
+    unglued_dict = {}
+    for indx in unglued_indices:
+        unglued_dict.update({indx: var_dict[indx]})
+
+    #print('unglued_dict: ')
+    #print(unglued_dict)
+
+    implicants_dict_pure.update(unglued_dict)
+
+    if not glued_indices and unglued_indices:
+        return var_dict
+
+    return var_dict.update(unglued_dict) \
+        if not find_implicants(implicants_dict_pure, variables, count - 1) \
+        else find_implicants(implicants_dict_pure, variables, count - 1)
+
+
+def find_optimal_coverage(implicants_dict: Dict) -> Dict:
+    """Finds the optimal implicants combination covering all the terms"""
+    indices = list(set(flatten(list(implicants_dict.keys()))))
+    #print('indices: ')
+    #print(indices)
+
+    combs = list(itertools.permutations(implicants_dict.keys(), len(implicants_dict)))
+
+    gr = []
+    vars = []
+    indices_set = set()
+    for index_combs in combs:
+        for index_comb in index_combs:
+            if isinstance(index_comb, int):
+                indices_set.update((index_comb,))
+            else:
+                indices_set.update(set(flatten(index_comb)))
+            vars.append(implicants_dict[index_comb])
+            if indices_set == set(indices):
+                gr.append(vars)
+                break
+        indices_set = set()
+        vars = []
+
+    #print('RESULT: ')
+    #print(min(gr, key=len))
+
+    #print(gr)
+
+    return min(gr, key=len)
+
+
+def flatten(list_to_flatten):
+    """Flatten the given nested structure"""
+    if not isinstance(list_to_flatten, (list, tuple)):
+        return list_to_flatten
+
+    for elem in list_to_flatten:
+        if isinstance(elem, (list, tuple)):
+            for x in flatten(elem):
+                yield x
+        else:
+            yield elem
--- app/index.py
+++ app/index.py
@@ -1,46 +1,76 @@
 import gnureadline
 import os
+import re
 import string
-from typing import List, Tuple
+from typing import Tuple, List, Dict, Callable
 from collections import defaultdict
-from graphviz import Digraph
+from graphviz import Digraph, Graph
+import pandas
+from itertools import product
 
 from docs import Docs
+from functions import flatten, get_mdnf
 
 
 MIN_NUMBER = 3
 MAX_NUMBER = 20
 TERMINATOR = '!'
 
+expression = ''
 
 os.system('clear')
 docs = Docs()
 print(docs.get_welcome_message())
 
 
-def check_number_input(user_number) -> bool:
+def check_number_input(user_number: str) -> bool:
+    """
+    Checks user's input for allowed symbols.
+    :param user_number:
+    :return:
+    """
     return user_number.isdigit() and MIN_NUMBER <= int(user_number) <= MAX_NUMBER
 
 
 def get_number() -> int:
+    """
+    Prompts the user to input number of vertices.
+    :return:
+    """
     user_input = input('Please, enter the number of vertices: ')
     if not check_number_input(user_input):
         raise Exception('Invalid number. Please, try again.')
     return int(user_input)
 
 
-def get_letters(count) -> Tuple:
+def get_letters(count: int) -> Tuple:
+    """
+    Return the letters range for given count.
+    :param count:
+    :return:
+    """
     return tuple(string.ascii_lowercase)[:count]
 
 
-def get_vertex_direction(name: str, vertices_names: Tuple):
+def get_vertex_direction(name: str, vertices_names: Tuple) -> str:
+    """
+    Prompts the user to input directed vertex name.
+    :param name:
+    :param vertices_names:
+    :return:
+    """
     user_input = input('{}: '.format(name))
     if user_input not in vertices_names and user_input != TERMINATOR:
         raise Exception('Invalid vertex name. Please, try again.')
     return user_input
 
 
-def get_adjacency_matrix(vertices_names: Tuple):
+def get_adjacency_matrix(vertices_names: Tuple) -> Dict:
+    """
+    Builds an adjacency matrix by given vertices.
+    :param vertices_names:
+    :return:
+    """
     print('Choose the edge directions: ')
     matrix = defaultdict(dict)
     for vertex_source in vertices_names:
@@ -60,7 +90,14 @@ def get_adjacency_matrix(vertices_names: Tuple):
     return matrix
 
 
-def get_floors(source_matrix, vertices_names: Tuple, source_floor=()):
+def get_floors(source_matrix: Dict, vertices_names: Tuple, source_floor=()) -> Tuple:
+    """
+    Calculates the graph floors (levels) to order it.
+    :param source_matrix:
+    :param vertices_names:
+    :param source_floor:
+    :return:
+    """
     #print('source_floor: ')
     #print(source_floor)
 
@@ -103,6 +140,93 @@ def get_floors(source_matrix, vertices_names: Tuple, source_floor=()):
         return floor + vertices_names_reduced
 
     return get_floors(source_matrix, vertices_names_reduced, floor)
+
+
+def get_complement_matrix(source_matrix: Dict, vertices_names: Tuple) -> Dict:
+    """
+    Build a complement adjacency_matrix for given matrix.
+    :param source_matrix:
+    :param vertices_names:
+    :return:
+    """
+    complement_matrix = defaultdict(dict)
+    for col_vertex in vertices_names:
+        for row_vertex in vertices_names:
+            if col_vertex == row_vertex:
+                break
+            if not source_matrix.get(row_vertex, {}).get(col_vertex, {}):
+                complement_matrix[row_vertex][col_vertex] = True
+
+    return complement_matrix
+
+
+def get_cnf_by_adjacency_matrix(source_matrix: Dict) -> str:
+    """
+    Build a CNF expression by adjacency matrix
+    :param source_matrix:
+    :return:
+    """
+    expression = ''
+    var_batch = []
+    common_batch = []
+    for row_vertex, vertices in source_matrix.items():
+        for col_vertex in vertices.keys():
+            common_batch.append([row_vertex, col_vertex])
+
+    #print(common_batch)
+
+    result_batch = []
+    for batch in common_batch:
+        result_batch.append('(' + ' or '.join(batch) + ')')
+
+    return ' and '.join(result_batch)
+
+
+def truth_table(func: Callable) -> pandas.DataFrame:
+    """
+    Build the truth table using given function
+    :param func:
+    :return:
+    """
+    values = [list(args) + [int(func(*args))] for args in product([0, 1], repeat=func.__code__.co_argcount)]
+    return pandas.DataFrame(
+        values,
+        columns=(list(func.__code__.co_varnames) + [expression]))
+
+
+def find_clique_graphs() -> List:
+    """
+    Finds clique graphs
+    :return:
+    """
+    cnf_expression = get_cnf_by_adjacency_matrix(complement_adjacency_matrix)
+    #print('cnf_expression: ')
+    #print(cnf_expression)
+
+    globals()['expression'] = cnf_expression
+
+    expr_variables = re.findall(r'([a-z])\sor\s([a-z])', expression)
+    expr_variables = sorted(set(flatten(expr_variables)))
+    #print('expr_variables: ')
+    #print(expr_variables)
+
+    exec(f"""def expression_function({','.join(expr_variables)}): return eval(expression)""", globals())
+
+    data = truth_table(expression_function)
+    #print(data)
+
+    mdnf_string, mdnf_list = get_mdnf(data, expression, expr_variables)
+
+    print('\r\nMDNF: ' + mdnf_string)
+    #
+    # print('\r\nVertices sets: ')
+    # for number, vertices_set in enumerate(mdnf_list, start=1):
+    #     print('{number}. {{{vertices}}}'.format(number=number, vertices=', '.join(vertices_set)))
+
+    #max_length = len(max(mdnf_list, key=len))
+    #max_graph = [item for item in mdnf_list if len(item) == max_length]
+
+    return mdnf_list
 
 
 try:
@@ -173,6 +297,18 @@ try:
     }
     """
 
+    """
+    # Clique example
+    vertices = tuple(string.ascii_lowercase)[:6]
+    adjacency_matrix = {
+        'a': {'b': True, 'c': True},
+        'b': {'d': True, 'e': True, 'f': True},
+        'c': {'e': True, 'f': True},
+        'd': {'e': True, 'f': True},
+        'e': {'f': True},
+    }
+    """
+
     floors = get_floors(adjacency_matrix.copy(), vertices)
 
     #print('adjacency_matrix: ')
@@ -210,6 +346,7 @@ try:
     """
 
     dot = Digraph()
+    graph = Graph(engine='sfdp')
     for floor in floors:
         edges = []
         if isinstance(floor, tuple):
@@ -218,6 +355,7 @@ try:
                     for direction_vertex, value in resulted_matrix[vertex].items():
                         edges.append(vertex + direction_vertex)
                     dot.edges(edges)
+                    graph.edges(edges)
                     edges = []
         else:
             if floor in resulted_matrix:
@@ -225,8 +363,23 @@ try:
                     edges.append(floor + direction_vertex)
         if edges:
             dot.edges(edges)
+            graph.edges(edges)
 
     dot.render('output/ordered_graph.gv')
+    graph.render('output/unordered_graph.gv')
+
+
+    complement_adjacency_matrix = get_complement_matrix(adjacency_matrix.copy(), vertices)
+    #print('complement_adjacency_matrix: ')
+    #print(dict(complement_adjacency_matrix))
+
+    if not complement_adjacency_matrix:
+        print('The complement adjacency matrix is empty. Cannot build complement graph.')
+    else:
+        clique_graphs = find_clique_graphs()
+        print('\r\nClique graph: ')
+        for num, vertices_set in enumerate(clique_graphs, start=1):
+            print('{number}. {{{vertices}}}'.format(number=num, vertices=', '.join(vertices_set)))
 
 
 except Exception as exception:
